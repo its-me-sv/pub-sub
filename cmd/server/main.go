7b53eb36f3ddbcd1ca3b97bf82d52d1b2501033c
Suraj Vijay <surajcpp115@gmail.com>
2026-08-30 22:55:07 +0530

    Exchanges and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,8 +18,17 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer conn.Close()
-
 	log.Println("RabbitMQ connected successfully!!")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalln("Failed to create channel")
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Failed to publish message")
+	}
 
 	sigChn := make(chan os.Signal, 1)
 	signal.Notify(sigChn, os.Interrupt)
