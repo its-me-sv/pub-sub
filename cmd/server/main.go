11ceacdbd21b13216c26c7b3d41ae871b6c9f4f2
Suraj Vijay <surajcpp115@gmail.com>
2026-08-30 12:24:29 +0530

    feat: implement RabbitMQ connection and signal handling in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	log.Println("RabbitMQ connected successfully!!")
+
+	sigChn := make(chan os.Signal, 1)
+	signal.Notify(sigChn, os.Interrupt)
+	if <-sigChn == os.Interrupt {
+		log.Println("Shutting down!!")
+	}
 }
