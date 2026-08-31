522e74d5461c85a7cf7b75656056a67951833ab3
Suraj Vijay <surajcpp115@gmail.com>
2026-09-01 00:39:45 +0530

    feat: implement RabbitMQ connection, queue declaration, and JSON publishing

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+	log.Println("RabbitMQ connected successfully!!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalln("Failed to get username")
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatalln("Failed to declare and bind queue")
+	}
+
+	sigChn := make(chan os.Signal, 1)
+	signal.Notify(sigChn, os.Interrupt)
+	if <-sigChn == os.Interrupt {
+		log.Println("Shutting down!!")
+	}
 }
